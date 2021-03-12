2b24b49cc692fdb2d3dc8372f58f5c5c8857de9d
KaylaKeung <keunt963@newschool.edu>
2021-03-13 02:00:45 +0800

    page1test

---
+++ about page/aboutjava.js
@@ -0,0 +1,7 @@
+document.querySelector("#menu-btn").addEventListener("click", function () {
+    document.querySelector("#nav-wrapper").classList.toggle("menu-closed");
+  });
+  document.querySelector(".one").addEventListener("click", function () {
+      document.querySelector("#nav-wrapper").classList.toggle("menu-closed");
+    });
+    
